de0fb55c6d3ba7e44fbc565bb5cdd3a00b48ee78
jzolo22 <jzolotarev@gmail.com>
2021-01-13 17:20:51 -0500

    added theming through semantic-ui

---
+++ src/components/SideBar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import {
+    Checkbox,
+    Grid,
+    Header,
+    Image,
+    Menu,
+    Ref,
+    Segment,
+    Sidebar,
+  } from 'semantic-ui-react'
+
+
+
+class SideBar extends React.Component {
+
+    render(){
+        return(
+            
+        )
+    }
+
+}
+
+export default SideBar
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'semantic-ui-css/semantic.min.css'
+import 'semantic-ui-less/semantic.less'
 import { BrowserRouter } from 'react-router-dom'
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
