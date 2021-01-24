fb132ebebfde01ec91579c64a71bbbed10d47f2b
jzolo22 <jzolotarev@gmail.com>
2021-01-24 12:03:21 -0500

    login page styling

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import { logIn } from '../redux/actions'
-import { Divider } from 'semantic-ui-react'
+import { Divider, Segment } from 'semantic-ui-react'
 
 class LoginPage extends React.Component {
 
@@ -25,22 +25,25 @@ class LoginPage extends React.Component {
         // console.log(this.state)
         return(
             <div style={{marginTop: "15%", textAlign: "center"}}>
-                <h1>Log In</h1>
-                <form class="ui form" onSubmit={this.onSubmit}>
-                    <div style={{margin: "0% 40% 0% 40%"}} class="field">
-                        <label>Username</label>
-                        <input  type="text" name="username" placeholder="Username" value={this.state.username}onChange={this.onChange}></input>
-                    </div>
-                    <div style={{margin: "0% 40% 1% 40%"}} class="field">
-                        <label>Password</label>
-                        <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.onChange}></input>
-                    </div>
-                    <button class="ui button" type="submit">Submit</button>
-                </form>
-                <Divider horizontal>or</Divider>
-                <NavLink to={'/users/new'}>
-                    Sign Up
-                </NavLink>
+                <Segment raised style={{margin: "0% 40%"}}>
+                    <h1>Log In</h1>
+                    <form class="ui form" onSubmit={this.onSubmit}>
+                        <div  class="field">
+                            <label>Username</label>
+                            <input  type="text" name="username" placeholder="Username" value={this.state.username}onChange={this.onChange}></input>
+                        </div>
+                        <div  class="field">
+                            <label>Password</label>
+                            <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.onChange}></input>
+                        </div>
+                        <button class="ui button" type="submit">Submit</button>
+                    </form>
+                </Segment>
+                
+                    <Divider horizontal style={{marginTop: "30px"}}>or</Divider>
+                    <NavLink to={'/users/new'}>
+                        Sign Up
+                    </NavLink>
             </div>
         
         )
